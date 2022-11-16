6566d1065a76e8884f2cce7356392eba27aac53d
SleepWalker <SleepWalker@123.com>
2022-11-16 10:59:42 +0800

    init

---
+++ src/main/java/com/sleep/myshow/MyshowApplication.java
@@ -0,0 +1,13 @@
+package com.sleep.myshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyshowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sleep/myshow/MyshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleep.myshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
