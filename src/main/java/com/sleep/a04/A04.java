9c70bfd60e5c1d07b82b6f9340f6a4ba8388cf6d
SleepWalker <SleepWalker@123.com>
2022-11-21 22:38:29 +0800

    4.2 @ConfigurationProperties

--- src/main/java/com/sleep/a04/A04.java
+++ src/main/java/com/sleep/a04/A04.java
@@ -1,6 +1,7 @@
 package com.sleep.a04;
 
 import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBindingPostProcessor;
 import org.springframework.context.annotation.CommonAnnotationBeanPostProcessor;
 import org.springframework.context.annotation.ContextAnnotationAutowireCandidateResolver;
 import org.springframework.context.support.GenericApplicationContext;
@@ -22,6 +23,7 @@ public class A04 {
         context.registerBean("bean1", Bean1.class);
         context.registerBean("bean2", Bean2.class);
         context.registerBean("bean3", Bean3.class);
+        context.registerBean("bean4", Bean4.class);
 
         //添加bean后处理器
         context.getDefaultListableBeanFactory().setAutowireCandidateResolver(new ContextAnnotationAutowireCandidateResolver());
@@ -29,8 +31,12 @@ public class A04 {
 
         context.registerBean(CommonAnnotationBeanPostProcessor.class); //@Resource @PostConstruct @PreDestroy
 
+        ConfigurationPropertiesBindingPostProcessor.register(context.getDefaultListableBeanFactory()); //@ConfigurationProperties
+
         //初始化容器
         context.refresh();  //执行BeanFactory后处理器，添加bean后处理器，初始化所有单例
+
+        System.out.println(context.getBean(Bean4.class));
 
         //销毁容器
         context.close();
